733c3c9f539ea2427490c3b11ef3dd15e0b59c30
Faisol173 <1999faisol173@gmail.com>
2022-11-29 21:22:39 +0600

    firebase done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,35 @@
 import "./loginStyle.css"
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/Firebase";
+import { queryAllByAttribute } from "@testing-library/react";
 
 export default function Login() {
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    console.log(email);
+  
+    const handleSignIn=async()=>{
+  
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          alert("Your are "+user.email);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
   return (
     <div className="main-wrap">
        <div className="outer-wrap">
           <h1>GHURA-GHURI</h1>
             <h3>Log in</h3>
             <hr/>
-            {/* <div class="social-login">
+            {/* <div className="social-login">
                 <a href="#">
                     <img src="../image/fb.png" alt=""/>
                 </a>
@@ -25,22 +47,22 @@ export default function Login() {
 
             
 
-            <form className="form">
-                <label for="email">Email</label>
+            <div className="form">
+                <label htmlFor="email">Email</label>
                 <br/>
-                <input type="email" name="email" id="email" placeholder="Email"/>
+                <input type="email" name="email" onChange={(e)=>setEmail(e.target.value)} id="email" placeholder="Email"/>
                 <br/>
-                <label for="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <br/>
-                <input type="password" name="password" id="password" placeholder="Password"/>
+                <input type="password" name="password" onChange={(e)=>setPassword(e.target.value)} id="password" placeholder="Password"/>
                 <br/>
                 <input type="checkbox" name="check" id="check"/>
-                <span class="rm-me">Remember Me</span>
-                <a href="#" class="fg-pa">Forgot Password?</a>
+                <span className="rm-me">Remember Me</span>
+                <a href="#" className="fg-pa">htmlForgot Password?</a>
                 <br/>
-                <button type="submit" class="btn">Log in</button>
-            </form>
-            <div class="terms">
+                <button type="submit" className="btn" onClick={handleSignIn}>Log in</button>
+            </div>
+            <div className="terms">
                 <a href="#">Privacy Policy</a>
                 <a href="#">Terms & Conditions</a>
             </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import "./SignupStyle.css"
+import { useState } from "react"
+import "./SignupStyle.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/Firebase";
 
-export default class SignUp extends Component {
-  render() {
-    return (
+const SignUp = ()=>{
+
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  console.log(email);
+
+  const handleSignup=async()=>{
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  }
+  return(
+     <div>
       <div>
-         <div class="main-wrap">
-           <div class="form-wrap">
+         <div className="main-wrap">
+           <div className="form-wrap">
                <h1>SIGN <span>UP</span></h1>
                <h6>Welcome to Ghura-ghuri</h6>
                
 
-               <form class="form">
-                   <input type="email" onchange="" name="email" id="email" placeholder="Email"/>
+               <div className="form">
+                   <input type="email" onChange={(e)=>setEmail(e.target.value)} name="email" id="email" placeholder="Email"/>
                     <br/>
-                   <input type="password" name="pass" id="pass" placeholder="Password"/>
+                   <input type="password" onChange={(e)=>setPassword(e.target.value)} name="pass" id="pass" placeholder="Password"/>
                     <br/>
                    <input type="password" name="cpass" id="cpass" placeholder="Confirm Password"/>
                     <br/>
-                   <button type="submit" class="form-btn">SIGN UP</button>
-               </form>
-               {/* <span class="sign-up-with">Or sign up with</span> */}
+                   <button type="submit" onClick={handleSignup} className="form-btn">SIGN UP</button>
+               </div>
+               {/* <span className="sign-up-with">Or sign up with</span> */}
 
                
-               {/* <div class="social-logo">
+               {/* <div className="social-logo">
                     <a href="#" title="Facebook">
                         <img src="../image/fb.png" alt="Facebook"/>
                     </a>
@@ -40,6 +64,7 @@ export default class SignUp extends Component {
            </div>
        </div>
       </div>
-    )
-  }
+     </div>
+  )
 }
+export default SignUp
